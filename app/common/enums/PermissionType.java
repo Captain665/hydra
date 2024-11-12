9b242a9718139b138cef0500eb374f84d30fa45f
loharmanish <loharmanish9571@gmail.com>
2024-11-12 09:06:38 +0530

    added customer delete path

--- app/common/enums/PermissionType.java
+++ app/common/enums/PermissionType.java
@@ -1,1 +1,1 @@
-package common.enums;public enum PermissionType {    CUSTOMER_LIST_READ,    CUSTOMER_READ,    CUSTOMER_UPDATE,    PNR_READ}
+package common.enums;public enum PermissionType {    CUSTOMER_LIST_READ,    CUSTOMER_READ,    CUSTOMER_UPDATE,    PNR_READ,    CUSTOMER_DELETE}
