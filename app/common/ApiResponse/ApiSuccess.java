cabf38867fc05ae0080d3b04b33ce41f1effdb1b
loharmanish <loharmanish9571@gmail.com>
2024-11-26 08:52:37 +0530

    fix formatting

--- app/common/ApiResponse/ApiSuccess.java
+++ app/common/ApiResponse/ApiSuccess.java
@@ -1,1 +1,1 @@
-package common.ApiResponse;import common.enums.Status;public class ApiSuccess extends ApiResponse {	public ApiSuccess(Object result) {		super(Status.SUCCESS, "", result);	}}
+package common.ApiResponse;import common.enums.Status;public class ApiSuccess extends ApiResponse {	public ApiSuccess(Object result) {		super(Status.SUCCESS, "", result);	}	//format}
