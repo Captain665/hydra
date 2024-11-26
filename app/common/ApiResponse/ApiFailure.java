78a9b0f76ccdc1450ff15c3cd33c7f83ee9874da
loharmanish <loharmanish9571@gmail.com>
2024-11-26 08:26:20 +0530

    fix formatting

--- app/common/ApiResponse/ApiFailure.java
+++ app/common/ApiResponse/ApiFailure.java
@@ -1,1 +1,1 @@
-package common.ApiResponse;import common.enums.Status;public class ApiFailure extends ApiResponse {	public ApiFailure(String message) {		super(Status.FAILURE, message, null);	}	// code formatting	// code again formatting}
+package common.ApiResponse;import common.enums.Status;public class ApiFailure extends ApiResponse {	public ApiFailure(String message) {		super(Status.FAILURE, message, null);	}	//update}
