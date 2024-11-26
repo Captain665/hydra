238c7ff7e72a1f0688df32724df8d775f0aeafcb
loharmanish <loharmanish9571@gmail.com>
2024-11-26 08:41:28 +0530

    fix formating

--- app/common/ApiResponse/ApiResponse.java
+++ app/common/ApiResponse/ApiResponse.java
@@ -1,1 +1,40 @@
-package common.ApiResponse;import common.enums.Status;public class ApiResponse {	public Status status;	public String message;	public Object result;	public ApiResponse(Status status, String message, Object result) {		this.status = status;		this.message = message;		this.result = result;	}	public Status getStatus() {		return status;	}	public void setStatus(Status status) {		this.status = status;	}	public String getMessage() {		return message;	}	public void setMessage(String message) {		this.message = message;	}	public Object getResult() {		return result;	}	public void setResult(Object result) {		this.result = result;	}}
+package common.ApiResponse;
+
+import common.enums.Status;
+
+public class ApiResponse {
+
+	public Status status;
+	public String message;
+	public Object result;
+
+	public ApiResponse(Status status, String message, Object result) {
+		this.status = status;
+		this.message = message;
+		this.result = result;
+	}
+
+	public Status getStatus() {
+		return status;
+	}
+
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public Object getResult() {
+		return result;
+	}
+
+	public void setResult(Object result) {
+		this.result = result;
+	}
+}
