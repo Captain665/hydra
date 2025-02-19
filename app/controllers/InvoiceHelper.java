1b1d1b63b6196fe30d6a7950f8b887ea96b13263
loharmanish <loharmanish9571@gmail.com>
2025-02-19 10:32:53 +0530

    fix heliyatra ticket converter

--- app/controllers/InvoiceHelper.java
+++ app/controllers/InvoiceHelper.java
@@ -1,30 +1,40 @@
 package controllers;
 
 import com.itextpdf.html2pdf.HtmlConverter;
-import play.libs.Json;
+import play.Logger;
+
+import play.mvc.Http;
 import play.mvc.Result;
 
 import java.io.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.concurrent.CompletionStage;
 
+import static java.util.concurrent.CompletableFuture.runAsync;
 import static java.util.concurrent.CompletableFuture.supplyAsync;
-import static play.mvc.Results.badRequest;
 import static play.mvc.Results.ok;
 
 public class InvoiceHelper {
+	private final Logger.ALogger logger = Logger.of("utilities.InvoiceHelper.converter");
 
-	public CompletionStage<Result> converter() {
-		try {
+	public CompletionStage<Result> converter(Http.Request request) {
+		return supplyAsync(() -> {
+			logger.info("[" + request.id() + "] " + " Getting request for PDF generator");
 			File htmlFile = new File("public/invoices/Heliyatra.html");
-			String pdfPath = "public/invoices/itext.pdf";
-			OutputStream pdfOutputStream = new FileOutputStream(pdfPath);
-			InputStream htmlInputStream = new FileInputStream(htmlFile);
-			HtmlConverter.convertToPdf(htmlInputStream, pdfOutputStream);
-			pdfOutputStream.close();
-			htmlInputStream.close();
-			return supplyAsync(() -> ok(new File("public/invoices/itext.pdf")));
-		} catch (Exception e) {
-			return supplyAsync(() -> badRequest(Json.toJson(e.getMessage())));
-		}
+			String outputDirectory = "public/invoices/";
+			String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"));
+			File outputPdfPath = new File(outputDirectory + "/invoice_" + timestamp + ".pdf");
+			runAsync(() -> {
+				try {
+					HtmlConverter.convertToPdf(new FileInputStream(htmlFile), new FileOutputStream(outputPdfPath));
+				} catch (Exception e) {
+					logger.error("[" + request.id() + "] " + "Error: " + e.getMessage());
+					e.printStackTrace();
+				}
+			});
+			logger.info("[" + request.id() + "] " + "Response	: Saving PDF at " + outputPdfPath);
+			return ok(new File("public/invoices/itext.pdf"));
+		});
 	}
 }
